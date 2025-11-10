0cf7d5bf4384a21d49b7c3410a0b80f0695511e8
Ilia Balandin <balandin.ilia.1@gmail.com>
2025-11-10 17:01:28 +0100

    Change TCP port value from 5555 to 40000

--- parameters/controls.cpp
+++ parameters/controls.cpp
@@ -6,7 +6,7 @@ static_assert(sizeof(IPAddr) == 4, "wrong type for IP address");
 const IPAddr addrIP = INADDR_LOOPBACK;
 
 static_assert(sizeof(TCPPort) == 2, "wrong type for TCP port");
-const TCPPort port = 5555;
+const TCPPort port = 40000;
 
 static_assert(sizeof(number) == 8, "wrong type of number type");
 
