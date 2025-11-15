c3c4529b17243eb6479d78417e6f185e70287258
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-11-15 09:42:08 +0100

    progress

--- client/mainC.cpp
+++ client/mainC.cpp
@@ -12,7 +12,7 @@ int main()
 
     sockaddr_in serverAddr{};
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(addrIP);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     serverAddr.sin_port = htons(port);
     connect(idSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr));
 
