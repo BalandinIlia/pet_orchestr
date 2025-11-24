14cfd12397316f735b2059898cea5ae059a3654c
Ilia Balandin <ilia.balandin@student.uva.nl>
2025-11-24 10:11:12 +0100

    renaming

--- cache_exec/build.sh
+++ cache_exec/build.sh
@@ -1,3 +1,3 @@
-g++ -c main.cpp
-g++ main.o -L../libraries -l:networking_library.a -l:networking_utils.a -l:logger.a -l:parameters.a -o cache_exec
+g++ -c main_cache.cpp
+g++ main_cache.o -L../libraries -l:networking_library.a -l:networking_utils.a -l:logger.a -l:parameters.a -o cache_exec
 mv cache_exec ../executables
--- network_exec/build.sh
+++ network_exec/build.sh
@@ -1,4 +1,4 @@
 g++ -c clientInstance.cpp
-g++ -c mainS.cpp
-g++ clientInstance.o mainS.o -L../libraries -l:networking_library.a -l:networking_utils.a -l:logger.a -l:parameters.a -o network_exec
+g++ -c main_network.cpp
+g++ clientInstance.o mainS_network.o -L../libraries -l:networking_library.a -l:networking_utils.a -l:logger.a -l:parameters.a -o network_exec
 mv network_exec ../executables
